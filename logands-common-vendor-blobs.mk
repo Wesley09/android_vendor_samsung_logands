64c15bbc32686bd426569ac12d3237105544c18a
ZIM <zim555@mail.ru>
2015-06-13 19:26:25 +0300

    Update lib

--- logands-common-vendor-blobs.mk
+++ logands-common-vendor-blobs.mk
@@ -1,84 +1,5 @@
 LOCAL_PATH := vendor/samsung/logands
 
-#$(LOCAL_PATH)/proprietary/lib/libvwengine.so:system/lib/libvwengine.so \
-#$(LOCAL_PATH)/proprietary/lib/libsamsungtts.so:system/lib/libsamsungtts.so
-#$(LOCAL_PATH)/proprietary/lib/libdeblock.so:system/lib/libdeblock.so \
-#$(LOCAL_PATH)/proprietary/bin/p2p_supplicant:system/bin/p2p_supplicant \
-#$(LOCAL_PATH)/proprietary/bin/usbdev_mode:system/bin/usbdev_mode \
-#$(LOCAL_PATH)/proprietary/lib/libsharevia.so:system/lib/libsharevia.so \
-#$(LOCAL_PATH)/proprietary/lib/libsharevia_jni.so:system/lib/libsharevia_jni.so \
-#$(LOCAL_PATH)/proprietary/lib/libsec_devenc.so:system/lib/libsec_devenc.so \
-#$(LOCAL_PATH)/proprietary/lib/libsec_ecryptfs.so:system/lib/libsec_ecryptfs.so \
-#$(LOCAL_PATH)/proprietary/lib/libsecface.so:system/lib/libsecface.so \
-#$(LOCAL_PATH)/proprietary/lib/libsecfips.so:system/lib/libsecfips.so \
-#$(LOCAL_PATH)/proprietary/lib/libsec_km.so:system/lib/libsec_km.so \
-#$(LOCAL_PATH)/proprietary/lib/libarcsoft_blink_detection.so:system/lib/libarcsoft_blink_detection.so \
-#$(LOCAL_PATH)/proprietary/lib/libarcsoft_detection_base.so:system/lib/libarcsoft_detection_base.so \
-#$(LOCAL_PATH)/proprietary/lib/libarcsoft_face_tracking.so:system/lib/libarcsoft_face_tracking.so \
-#$(LOCAL_PATH)/proprietary/lib/libarcsoft_panorama_burstcapture.so:system/lib/libarcsoft_panorama_burstcapture.so \
-#$(LOCAL_PATH)/proprietary/lib/libarcsoft_smile_detection.so:system/lib/libarcsoft_smile_detection.so \
-#$(LOCAL_PATH)/proprietary/lib/libfacerecognition.so:system/lib/libfacerecognition.so \
-#$(LOCAL_PATH)/proprietary/lib/libfacetracker.so:system/lib/libfacetracker.so \
-#$(LOCAL_PATH)/proprietary/lib/libgs.so:system/lib/libgs.so \
-#$(LOCAL_PATH)/proprietary/lib/libimage.so:system/lib/libimage.so \
-#$(LOCAL_PATH)/proprietary/lib/libimgtops.so:system/lib/libimgtops.so \
-#$(LOCAL_PATH)/proprietary/lib/libnmsp_vocoder_speex.so:system/lib/libnmsp_vocoder_speex.so \
-#$(LOCAL_PATH)/proprietary/lib/libnvaccessor_fb.so:system/lib/libnvaccessor_fb.so \
-#$(LOCAL_PATH)/proprietary/lib/libomadrm.so:system/lib/libomadrm.so \
-#$(LOCAL_PATH)/proprietary/lib/libquramagifencoder.so:system/lib/libquramagifencoder.so \
-#$(LOCAL_PATH)/proprietary/lib/libquramimagecodec.so:system/lib/libquramimagecodec.so \
-#$(LOCAL_PATH)/proprietary/lib/libui3d-jni.so:system/lib/libui3d-jni.so \
-#$(LOCAL_PATH)/proprietary/lib/libvt_codec.so:system/lib/libvt_codec.so \
-#$(LOCAL_PATH)/proprietary/lib/libvtmanager.so:system/lib/libvtmanager.so \
-#$(LOCAL_PATH)/proprietary/lib/libvtstack.so:system/lib/libvtstack.so
-#$(LOCAL_PATH)/proprietary/lib/libvtqinit.so:system/lib/libvtqinit.so \
-#$(LOCAL_PATH)/proprietary/lib/libsavsac.so:system/lib/libsavsac.so \
-#$(LOCAL_PATH)/proprietary/lib/libsavsff.so:system/lib/libsavsff.so \
-#$(LOCAL_PATH)/proprietary/lib/libsavsmeta.so:system/lib/libsavsmeta.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextavi.so:system/lib/libsfextavi.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextcmn.so:system/lib/libsfextcmn.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextcp.so:system/lib/libsfextcp.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextflac.so:system/lib/libsfextflac.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextflv.so:system/lib/libsfextflv.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextmkv.so:system/lib/libsfextmkv.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextrmf.so:system/lib/libsfextrmf.so \
-#$(LOCAL_PATH)/proprietary/lib/libsfextwmf.so:system/lib/libsfextwmf.so \
-#$(LOCAL_PATH)/proprietary/bin/at_distributor:system/bin/at_distributor \
-#$(LOCAL_PATH)/proprietary/bin/atx:system/bin/atx \
-#$(LOCAL_PATH)/proprietary/bin/atxd:system/bin/atxd \
-#$(LOCAL_PATH)/proprietary/bin/npsmobex:system/bin/npsmobex \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_mp3dec.so:system/lib/libstagefright_soft_mp3dec.so
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_amrnb_common.so:system/lib/libstagefright_amrnb_common.so \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_amrdec.so:system/lib/libstagefright_soft_amrdec.so \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_amrnbenc.so:system/lib/libstagefright_soft_amrnbenc.so \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_amrwbenc.so:system/lib/libstagefright_soft_amrwbenc.so
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_aacdec.so:system/lib/libstagefright_soft_aacdec.so \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_aacenc.so:system/lib/libstagefright_soft_aacenc.so
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_mpeg4dec.so:system/lib/libstagefright_soft_mpeg4dec.so \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_mpeg4enc.so:system/lib/libstagefright_soft_mpeg4enc.so
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_h264dec.so:system/lib/libstagefright_soft_h264dec.so \
-#$(LOCAL_PATH)/proprietary/lib/libstagefright_soft_h264enc.so:system/lib/libstagefright_soft_h264enc.so
-#$(LOCAL_PATH)/proprietary/lib/libImmVibeJ.so:system/lib/libImmVibeJ.so \
-#$(LOCAL_PATH)/proprietary/lib/libsensorservice.so:system/lib/libsensorservice.so \
-#$(LOCAL_PATH)/proprietary/lib/libbinder.so:system/lib/libbinder.so \
-#$(LOCAL_PATH)/proprietary/lib/libutils.so:system/lib/libutils.so \
-#$(LOCAL_PATH)/proprietary/etc/srm.bin:system/etc/srm.bin \
-#$(LOCAL_PATH)/proprietary/lib/hw/audio_policy.hawaii.so:system/lib/hw/audio_policy.hawaii.so \
-#$(LOCAL_PATH)/proprietary/lib/libaudiopolicy_sec.so:system/lib/libaudiopolicy_sec.so \
-#$(LOCAL_PATH)/proprietary/bin/atrace:system/bin/atrace \
-#$(LOCAL_PATH)/proprietary/lib/libseccameracore.so:system/lib/libseccameracore.so \
-#$(LOCAL_PATH)/proprietary/lib/libseccamera_jni.so:system/lib/libseccamera_jni.so \
-#$(LOCAL_PATH)/proprietary/lib/libarccamera.so:system/lib/libarccamera.so \
-#$(LOCAL_PATH)/proprietary/lib/libseccameraeffect.so:system/lib/libseccameraeffect.so \
-#$(LOCAL_PATH)/proprietary/lib/libsecimaging.so:system/lib/libsecimaging.so \
-#$(LOCAL_PATH)/proprietary/lib/libsecjpeginterface.so:system/lib/libsecjpeginterface.so \
-#$(LOCAL_PATH)/proprietary/lib/libexif_jni.so:system/lib/libexif_jni.so \
-#$(LOCAL_PATH)/proprietary/etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
-#$(LOCAL_PATH)/proprietary/etc/bluetooth/main.conf:system/etc/bluetooth/main.conf \
-#$(LOCAL_PATH)/proprietary/lib/libsecmediarecorder_jni.so:system/lib/libsecmediarecorder_jni.so
-
-
-
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/bin/BCM4330B1_002.001.003.0967.1173.hcd:system/bin/BCM4330B1_002.001.003.0967.1173.hcd \
 	$(LOCAL_PATH)/proprietary/bin/bkmgrd:system/bin/bkmgrd \
@@ -95,99 +16,35 @@ PRODUCT_COPY_FILES += \
 # mp3 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/lib/libbrcmmp3dec.so:system/lib/libbrcmmp3dec.so \
-	$(LOCAL_PATH)/proprietary/lib/libbrcmmp3enc.so:system/lib/libbrcmmp3enc.so \
 	$(LOCAL_PATH)/proprietary/lib/libmp3_dec_dummy.so:system/lib/libmp3_dec_dummy.so \
-	$(LOCAL_PATH)/proprietary/lib/libmp3_enc_dummy.so:system/lib/libmp3_enc_dummy.so \
 	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.mp3.decoder.so:system/lib/libOMX.brcm.audio.mp3.decoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.mp3.encoder.so:system/lib/libOMX.brcm.audio.mp3.encoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxmp3d.so:system/lib/libsomxmp3d.so
 	
 # amr 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/lib/libbrcmamrnbaridec.so:system/lib/libbrcmamrnbaridec.so \
-	$(LOCAL_PATH)/proprietary/lib/libbrcmamrnbarienc.so:system/lib/libbrcmamrnbarienc.so \
 	$(LOCAL_PATH)/proprietary/lib/libbrcmamrwbaridec.so:system/lib/libbrcmamrwbaridec.so \
-	$(LOCAL_PATH)/proprietary/lib/libbrcmamrwbarienc.so:system/lib/libbrcmamrwbarienc.so \
 	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.amrnb.decoder.so:system/lib/libOMX.brcm.audio.amrnb.decoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.amrnb.encoder.so:system/lib/libOMX.brcm.audio.amrnb.encoder.so \
 	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.amrwb.decoder.so:system/lib/libOMX.brcm.audio.amrwb.decoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.amrwb.encoder.so:system/lib/libOMX.brcm.audio.amrwb.encoder.so \
 	$(LOCAL_PATH)/proprietary/lib/libsAMRNB.so:system/lib/libsAMRNB.so \
-	$(LOCAL_PATH)/proprietary/lib/libsAMRWB.so:system/lib/libsAMRWB.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxamrd.so:system/lib/libsomxamrd.so
+	$(LOCAL_PATH)/proprietary/lib/libsAMRWB.so:system/lib/libsAMRWB.so
 	
 # aac 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libaacdecoderwrapper.so:system/lib/libaacdecoderwrapper.so \
-	$(LOCAL_PATH)/proprietary/lib/libaac_enc_dummy.so:system/lib/libaac_enc_dummy.so \
-	$(LOCAL_PATH)/proprietary/lib/libbrcmaacenc.so:system/lib/libbrcmaacenc.so \
-	$(LOCAL_PATH)/proprietary/lib/libbrcmheaacdecoder.so:system/lib/libbrcmheaacdecoder.so \
 	$(LOCAL_PATH)/proprietary/lib/libFraunhoferAAC.so:system/lib/libFraunhoferAAC.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.aac.decoder.so:system/lib/libOMX.brcm.audio.aac.decoder.so \
-    $(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.aac.encoder.so:system/lib/libOMX.brcm.audio.aac.encoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libsfextaac.so:system/lib/libsfextaac.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxaacd.so:system/lib/libsomxaacd.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxaace.so:system/lib/libsomxaace.so
-	
-# mpeg4
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libmpeg4_dec_asp.so:system/lib/libmpeg4_dec_asp.so \
-	$(LOCAL_PATH)/proprietary/lib/libmpeg4_dummy_enc.so:system/lib/libmpeg4_dummy_enc.so \
-	$(LOCAL_PATH)/proprietary/lib/libmpeg4_enc_sp.so:system/lib/libmpeg4_enc_sp.so \
-	$(LOCAL_PATH)/proprietary/lib/libmpeg4_fake_dec.so:system/lib/libmpeg4_fake_dec.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.mpeg4.decoder.so:system/lib/libOMX.brcm.video.mpeg4.decoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.mpeg4.encoder.so:system/lib/libOMX.brcm.video.mpeg4.encoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.mpeg4.hw.decoder.so:system/lib/libOMX.brcm.video.mpeg4.hw.decoder.so
+	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.audio.aac.decoder.so:system/lib/libOMX.brcm.audio.aac.decoder.so
 	
 # h264
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libh264_dec_bp.so:system/lib/libh264_dec_bp.so \
-	$(LOCAL_PATH)/proprietary/lib/libh264_dec_hp.so:system/lib/libh264_dec_hp.so \
-	$(LOCAL_PATH)/proprietary/lib/libh264_dummy_enc.so:system/lib/libh264_dummy_enc.so \
-	$(LOCAL_PATH)/proprietary/lib/libh264_enc_bp.so:system/lib/libh264_enc_bp.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.h264.decoder.so:system/lib/libOMX.brcm.video.h264.decoder.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.h264.encoder.so:system/lib/libOMX.brcm.video.h264.encoder.so \
 	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.h264.hw.decoder.so:system/lib/libOMX.brcm.video.h264.hw.decoder.so \
 	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.h264.hw.encoder.so:system/lib/libOMX.brcm.video.h264.hw.encoder.so
-	
-# h263
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libh263_dec_p3.so:system/lib/libh263_dec_p3.so \
-	$(LOCAL_PATH)/proprietary/lib/libh263_dummy_enc.so:system/lib/libh263_dummy_enc.so \
-	$(LOCAL_PATH)/proprietary/lib/libh263_enc_p3.so:system/lib/libh263_enc_p3.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.h263.decoder.so:system/lib/libOMX.brcm.video.h263.decoder.so \
-    $(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.h263.encoder.so:system/lib/libOMX.brcm.video.h263.encoder.so
-	
-# wmv	
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libsomxwmv7d.so:system/lib/libsomxwmv7d.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxwmv8d.so:system/lib/libsomxwmv8d.so \
-	$(LOCAL_PATH)/proprietary/lib/libwmv_dec_mp.so:system/lib/libwmv_dec_mp.so \
-	$(LOCAL_PATH)/proprietary/lib/libwmv_dec_sp.so:system/lib/libwmv_dec_sp.so
-	
-# wma	
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libsomxwmad.so:system/lib/libsomxwmad.so
 
 # other
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/lib/libvpx_dec.so:system/lib/libvpx_dec.so \
 	$(LOCAL_PATH)/proprietary/lib/libstagefrighthw.so:system/lib/libstagefrighthw.so \
-	$(LOCAL_PATH)/proprietary/lib/libtiming.so:system/lib/libtiming.so \
 	$(LOCAL_PATH)/proprietary/lib/libBrcmOMX_Component.so:system/lib/libBrcmOMX_Component.so \
-    $(LOCAL_PATH)/proprietary/lib/libBrcmOMX_Core.so:system/lib/libBrcmOMX_Core.so \
-	$(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.dummy.decoder.so:system/lib/libOMX.brcm.video.dummy.decoder.so \
-    $(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.dummy.encoder.so:system/lib/libOMX.brcm.video.dummy.encoder.so \
-    $(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.vc1.hw.decoder.so:system/lib/libOMX.brcm.video.vc1.hw.decoder.so \
-    $(LOCAL_PATH)/proprietary/lib/libOMX.brcm.video.vpx.decoder.so:system/lib/libOMX.brcm.video.vpx.decoder.so \
+    $(LOCAL_PATH)/proprietary/lib/libBrcmOMX_Core.so:system/lib/libBrcmOMX_Core.so
 	$(LOCAL_PATH)/proprietary/lib/libcodec_util.so:system/lib/libcodec_util.so \
     $(LOCAL_PATH)/proprietary/lib/libhwcodec.so:system/lib/libhwcodec.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxvp8d.so:system/lib/libsomxvp8d.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxsr263d.so:system/lib/libsomxsr263d.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxmp43d.so:system/lib/libsomxmp43d.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxflacd.so:system/lib/libsomxflacd.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxcore.so:system/lib/libsomxcore.so \
-	$(LOCAL_PATH)/proprietary/lib/libsomxcmn.so:system/lib/libsomxcmn.so \
 	$(LOCAL_PATH)/proprietary/lib/libuecodec.so:system/lib/libuecodec.so
 	
 PRODUCT_COPY_FILES += \
@@ -277,7 +134,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/lib/libISP.so:system/lib/libISP.so \
     $(LOCAL_PATH)/proprietary/lib/libisp2.so:system/lib/libisp2.so \
 	$(LOCAL_PATH)/proprietary/lib/libisp3.so:system/lib/libisp3.so \
-	$(LOCAL_PATH)/proprietary/lib/libexifa.so:system/lib/libexifa.so \
 	$(LOCAL_PATH)/proprietary/lib/libjpega.so:system/lib/libjpega.so \
 	
 PRODUCT_COPY_FILES += \
