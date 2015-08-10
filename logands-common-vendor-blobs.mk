fea54854129674b5c85a416931d707539f8158aa
ZIM <zim555@mail.ru>
2015-08-10 22:41:04 +0300

    Delete not need libs

--- logands-common-vendor-blobs.mk
+++ logands-common-vendor-blobs.mk
@@ -141,12 +141,8 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/lib/libjpega.so:system/lib/libjpega.so \
 	
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/proprietary/bin/cate_rpc_util:system/bin/cate_rpc_util \
-	$(LOCAL_PATH)/proprietary/lib/libcate_rpc.so:system/lib/libcate_rpc.so \
-	$(LOCAL_PATH)/proprietary/bin/usb_portd:system/bin/usb_portd \
 	$(LOCAL_PATH)/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
-	$(LOCAL_PATH)/proprietary/lib/libusb_config.so:system/lib/libusb_config.so \
-	$(LOCAL_PATH)/proprietary/etc/usb_portd.conf:system/etc/usb_portd.conf
+	$(LOCAL_PATH)/proprietary/lib/libusb_config.so:system/lib/libusb_config.so
 	
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/bin/playlpm:system/bin/playlpm \
